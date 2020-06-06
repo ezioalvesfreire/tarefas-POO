9779785705b15d2ad449db70ccddbd61eb21fa53
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-06-06 00:45:35 -0300

    Programa em java Jogo da velha

---
+++ jogoVelha/src/jogovelha/JogoVelha.java
@@ -0,0 +1,64 @@
+package jogovelha;
+
+import java.util.Scanner;
+public class JogoVelha {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner ler = new Scanner(System.in);
+		Tabuleiro jogo = new Tabuleiro();
+		String posicao;
+		int valida = 0, Jogadas = 0;
+		
+		while(true) {
+			System.out.println("============ Bem Vindo Ao Jogo da Velha de POO!!! ============");
+			jogo.Mostrar();
+			
+			do {//Starta jogo com jogador 1
+				System.out.println("Jogador 1 - Inicie o jogo informando uma posição: ");
+				posicao = ler.next();
+				while(!jogo.Valido(posicao)) {
+					System.out.println("Jogada Inválida");
+					System.out.println("Jogador 1 - Inicie o jogo informando uma posição: ");
+					posicao = ler.next();
+					valida = 0;
+				}
+				jogo.Jogada(posicao, "X");
+				valida = 1;
+				
+			}while(valida == 0); //finaliza jogada do jogador 1
+			
+			Jogadas++;
+			valida = 0;
+			jogo.Mostrar();
+			
+			if(!jogo.Ganhou(Jogadas).equals("null")) {
+				break;
+			}
+			do {//Starta jogo com jogador 2
+				System.out.println("Jogador 2 - Inicie o jogo informando uma posição: ");
+				posicao = ler.next();
+				while(!jogo.Valido(posicao)) {
+					System.out.println("Jogada Inválida");
+					System.out.println("Jogador 2 - Inicie o jogo informando uma posição: ");
+					posicao = ler.next();
+					valida = 0;
+				}
+				jogo.Jogada(posicao, "O");
+				valida = 1;
+				
+			}while(valida == 0); //finaliza jogada do jogador 2
+			
+			Jogadas++;
+			valida = 0;
+			jogo.Mostrar();
+			
+			if(!jogo.Ganhou(Jogadas).equals("null")) {
+				break;
+			}
+		}
+		System.out.println("*** PARABÉNS " + jogo.Ganhou(Jogadas) + " É O VENCEDOR!!!!! ***");
+        
+    }
+    
+}
---
+++ jogoVelha/src/jogovelha/Tabuleiro.java
@@ -0,0 +1,85 @@
+package jogovelha;
+
+public class Tabuleiro {
+    //Matriz declarada com posições, encapsulada com private
+		private String[][] tab = 	{{"1","2","3"}, 
+						{"4","5","6"}, 
+						{"7","8","9"}}; 
+		
+		//Metodo para mostrar a Matriz onde i = linha e j = coluna 
+		public String Mostrar() {
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					System.out.printf("   " + tab [i][j]); //concatena linha e coluna
+				}
+				System.out.println("\n"); //quebra de linha para mostrar com layout do tabuleiro
+			}
+			return null;
+		}
+		//-------------------Validação jogadas-------------------
+		public boolean Valido(String v) {
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					if(tab[i][j].equals(v)) //utilizado equals para comparação entre strings
+						return true;
+				}
+			}
+			return false;
+		}
+		
+		//--------------Insere "X" ou "O" nas jogadas--------------
+		public void Jogada(String p, String j) { //metodo usa como parametro p = posição (validada) e j = jogador
+			if(p.equals("1"))
+				tab[0][0] = j; //se posição 0-0 (ou posição 1) for digitada, vai receber o sinal do jogador (X ou 0)
+			else if(p.equals("2"))
+				tab[0][1] = j;
+			else if(p.equals("3"))
+				tab[0][2] = j;
+			else if(p.equals("4"))
+				tab[1][0] = j;
+			else if(p.equals("5"))
+				tab[1][1] = j;
+			else if(p.equals("6"))
+				tab[1][2] = j;
+			else if(p.equals("7"))
+				tab[2][0] = j;
+			else if(p.equals("8"))
+				tab[2][1] = j;
+			else if(p.equals("9"))
+				tab[2][2] = j;
+		}
+		
+		//--------------Verifica ganhador do jogo--------------
+		public String Ganhou(int jogadas) { //metodo de verificação das jogadas para identificar se jogador ganhou
+			String[] T = new String [8];//vetor para checar (T)otal das possibilidades onde jogador pode ganhar
+			String vencedor = "null";
+			if(jogadas == 9) {
+				vencedor = "----------- DEU VELHA!!! -----------";
+			}
+			//verificação das linhas
+			T[0] = tab[0][0] + tab[0][1] + tab[0][2];
+			T[1] = tab[1][0] + tab[1][1] + tab[1][2];
+			T[2] = tab[2][0] + tab[2][1] + tab[2][2];
+			
+			//verificação das colunas
+			T[3] = tab[0][0] + tab[1][0] + tab[2][0];
+			T[4] = tab[0][1] + tab[1][1] + tab[2][1];
+			T[5] = tab[0][2] + tab[1][2] + tab[2][2];
+			
+			//verificação das diagonais
+			T[6] = tab[0][0] + tab[1][1] + tab[2][2];
+			T[7] = tab[0][2] + tab[1][1] + tab[2][0];
+			
+			for(int i = 0; i<T.length; i++) { //varrer o vetor para identificar ganhador
+				if(T[i].equals("XXX")) {
+					vencedor = "Jogador 1";
+				}else if(T[i].equals("OOO")) {
+					vencedor = "Jogador 2";
+				}
+			}
+			return vencedor;
+		}
+    
+    
+    
+}
